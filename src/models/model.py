012b3021ee650af4b1b6ad2c6f98d8a7287caa39
Mac <kaaso@space.dtu.dk>
2022-09-29 10:40:34 -0230

    added stuff

--- src/models/model.py
+++ src/models/model.py
@@ -2,6 +2,7 @@ from ._callbacks import *
 from ._util import *
 from .model_arcitectures._model_quantile_simp import quantile_model
 from .model_arcitectures._model_quantule_v2 import quantile_model as qmodel_v2
+from .model_arcitectures._fcn_quantile import fcn_quantile
 
 
 class Model:
@@ -44,6 +45,7 @@ class Model:
         early_stopping: int = 500,
     ):
         """ """
+        self.which_model = which_model
         clean_session()
         self.batch_size = batch_size
         self.validation_split = validation_split
@@ -74,6 +76,19 @@ class Model:
                 Dropout_tcnn=0.10, init_lr=init_lr, batch_size=batch_size
             )
 
+        if which_model.lower() == "fcn_quantile":
+            print('quantile')
+            self.model = fcn_quantile(
+                input_size_img=(None,None,self.train_images[0].shape[-1]),
+                input_size_metadata=self.train_metadata[0, -2:].shape,
+                name=f"{model_name}",
+            )
+            self.model.make_model(
+                Dropout_tcnn=0.10, init_lr=init_lr, batch_size=batch_size
+            )
+
+
+
         self.callbacks = get_callbacks(
             self.model,
             optimizer_method="cosine",
@@ -90,8 +105,32 @@ class Model:
 
     def model_train(self, epochs: int = 50, verbose: int = 1):
         self.epochs = epochs
-        training_history = self.model.model.fit(
+        
+
+        if self.which_model.lower() == "fcn_quantile":
+            training_history = self.model.model.fit(
             [self.train_images, self.train_metadata[:, -2:]],
+            [self.train_target[0], self.train_target[1], self.train_target[2]],
+            batch_size=self.batch_size,
+            epochs=epochs,
+            verbose=verbose)
+        
+
+
+        else:
+            training_history = self.model.model.fit(
+                [self.train_images, self.train_metadata[:, -2:]],
+                [self.train_target[:, 0], self.train_target[:, 1], self.train_target[:, 2]],
+                batch_size=self.batch_size,
+                epochs=epochs,
+                validation_split=self.validation_split,
+                verbose=verbose)
+        self.training_history = training_history
+
+    def model_train_v2(self, epochs: int = 50, verbose: int = 1):
+        self.epochs = epochs
+        training_history = self.model.model.fit(
+            [self.train_images_vv,self.train_images_vh, self.train_metadata[:, -2:]],
             [self.train_target[:, 0], self.train_target[:, 1], self.train_target[:, 2]],
             batch_size=self.batch_size,
             epochs=epochs,
